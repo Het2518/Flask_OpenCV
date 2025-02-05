4cec79adfda94681d0c5144f4dbb37eac29efb64
Het Monpara <hetmonpara2022@gmail.com>
2025-02-05 14:45:59 +0530

    update app

--- app.py
+++ app.py
@@ -1,49 +1,88 @@
+
+
 from flask import Flask, render_template, Response
 import cv2
-import os
-
-app = Flask(__name__)
-
+import face_recognition
+import numpy as np
+app=Flask(__name__)
 camera = cv2.VideoCapture(0)
 
-# Load Haar cascades
-haarcascade_path = os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml')
-eye_cascade_path = os.path.join(cv2.data.haarcascades, 'haarcascade_eye.xml')
+Het_image = face_recognition.load_image_file("Het/Het.jpg")
+Het_face_encoding = face_recognition.face_encodings(Het_image)[0]
 
-face_detector = cv2.CascadeClassifier(haarcascade_path)
-eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
+Shriji_image = face_recognition.load_image_file("Shriji/Shriji.jpg")
+Shriji_face_encoding = face_recognition.face_encodings(Shriji_image)[0]
 
-def generate_frames():
+
+known_face_encodings = [
+    Het_face_encoding,
+    Shriji_face_encoding
+]
+known_face_names = [
+    "Het",
+    "Shriji"
+]
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+def gen_frames():  
     while True:
-        success, frame = camera.read()
+        success, frame = camera.read()  # read the camera frame
         if not success:
             break
         else:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_detector.detectMultiScale(gray, 1.1, 7)
+            # Resize frame of video to 1/4 size for faster face recognition processing
+            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_small_frame = small_frame[:, :, ::-1]
 
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                roi_gray = gray[y:y + h, x:x + w]
-                roi_color = frame[y:y + h, x:x + w]
+            # Only process every other frame of video to save time
+           
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
-                eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 2)
-                for (ex, ey, ew, eh) in eyes:
-                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                face_names.append(name)
+            
+
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__=='__main__':
     app.run(debug=True)
